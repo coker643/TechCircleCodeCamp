c88c8943e61fea8570a25851542fceab8f8e4d3f
coker643 <cokersamuel208@gmail.com>
2024-08-28 13:49:33 +0100

    Add files via upload

---
+++ mypython/myFirstCode.py
@@ -0,0 +1,9 @@
+#create a variable called password assign it to a value and get 
+#an input from a user with a message passordthen assign it to a
+# variable np check if np is equal to password
+
+
+password = "coker jnr"
+np = input("password: ")
+if password == np:
+	print("password correct")
